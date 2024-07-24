a0603826372620a6e4343c32e43e2eab65cc22be
Abdullah S. Khan <abdullahshamskhan@gmail.com>
2024-07-24 18:11:34 -0400

    Extract data from html tags using requests and beautiful soup libraries

--- task/scraper.py
+++ task/scraper.py
@@ -1,11 +1,20 @@
 import requests
+from bs4 import BeautifulSoup
 
 url = input('Input the URL:')
-
 r = requests.get(url)
-print(r.content)
 
-if r.status_code == 200 and 'content' is r.json():
-    print(r.json()['content'])
+if r.status_code == 200 and url.startswith('https://www.nature.com/articles/'):
+
+    soup = BeautifulSoup(r.content, 'html.parser')
+    title = soup.title.string
+    description = soup.find('meta', {'name' : 'description'})['content']
+
+    article_info = {
+        "title": title,
+        "description": description
+    }
+
+    print(article_info)
 else:
-    print('Invalid quote resource!')
+    print('Invalid page!')
