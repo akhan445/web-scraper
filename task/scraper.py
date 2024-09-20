b1341a1b7a68b9cfb0697b6b8f279981fa13237d
Abdullah S. Khan <abdullahshamskhan@gmail.com>
2024-09-20 17:18:25 -0400

    Perform webscraping on website. Extract articles from webpage and save each with category of News as a text file.

--- task/scraper.py
+++ task/scraper.py
@@ -1,14 +1,53 @@
+import string
+
 import requests
 from bs4 import BeautifulSoup
 from http import HTTPStatus
 
-url = input('Input the URL:')
+
+def modify_title(title_string):
+    modified_title = title_string.strip()
+    # for c in title_string.strip():
+    #     if c not in string.punctuation:
+    #         print(c)
+    #         modified_title += c
+
+    modified_title = modified_title.translate(str.maketrans('', '', string.punctuation))
+    modified_title = modified_title.replace(" ", "_")
+
+    modified_title += ".txt"
+
+    return modified_title
+
+
+url = "https://www.nature.com/nature/articles?sort=PubDate&year=2020&page=3"
 r = requests.get(url)
+saved_artciles = []
 
 if r.status_code == HTTPStatus.OK:
+    soup = BeautifulSoup(r.content, 'html.parser')
 
-    with open('source.html', 'wb') as file:
-        file.write(bytes(r.content))
-        print("Content saved.")
+    articles = soup.find_all('article')
+
+    for i in range(len(articles)):
+        article_type = articles[i].find('span', {"data-test": "article.type"}).text
+
+        if article_type.strip().lower() == "news":
+            article_link = articles[i].find('a', {"data-track-action": "view article"})['href']
+
+            # print("https://www.nature.com" + article_link)
+            article_req = requests.get("https://www.nature.com" + article_link)
+
+            if article_req.status_code == HTTPStatus.OK:
+                article_soup = BeautifulSoup(article_req.content, 'html.parser')
+
+                filename = modify_title(article_soup.title.string)
+                article_body = article_soup.find("p", {"class": "article__teaser"})
+                if article_body:
+                    with open(filename, "w") as file:
+                        file.write(article_body.text)
+                        saved_artciles.append(filename)
+
+    print("Saved articles: ", saved_artciles)
 else:
     print('The URL returned ', r.status_code)
